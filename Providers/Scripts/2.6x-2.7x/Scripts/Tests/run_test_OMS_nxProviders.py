3aba719d9b494ac342cc29247e71bd1c5d3b51af
eric@voxon.co <kadath@gmail.com>
2016-10-04 18:39:26 +0000

    Interim oms

--- Providers/Scripts/2.6x-2.7x/Scripts/Tests/run_test_OMS_nxProviders.py
+++ Providers/Scripts/2.6x-2.7x/Scripts/Tests/run_test_OMS_nxProviders.py
@@ -8,6 +8,8 @@ import glob
 #import pdb ; pdb.set_trace()
 
 os.system('rm /tmp/omstest_cleanup* 2> /dev/null')
+os.system('rm -rf /tmp/omsext 2> /dev/null')
+
 print 'Running test_OMS_nxProviders.py'
 #Check and return if present
 if not os.path.exists('/opt/micosoft/omsconfig'):
@@ -18,20 +20,24 @@ if not os.path.exists('/opt/micosoft/omsconfig'):
     bundle = glob.glob(bundle)[0]
     print 'Target = ' + bundle
     os.system('mkdir /tmp/omsext; cd /tmp/omsext ; ' + bundle + ' --extract')
-    if os.system('which dpkg-deb') == 0:
+    if os.system('which dpkg-deb &> /dev/null') == 0:
         for pkg in glob.glob('/tmp/omsext/omsbundle*/100/*.deb'):
             os.system('dpkg-deb -x ' + pkg + ' /tmp/omsconfig')
     else:
         for pkg in glob.glob('/tmp/omsext/100/*.rpm'):
             os.system('cd /tmp/omsconfig; /rpm2cpio ' + pkg + ' | cpio -idmv')
+    os.chdir('/tmp/omsconfig/opt/microsoft/omsconfig/module_packages')
+    for pkg in glob.glob('./*.zip'):
+        os.system('unzip ' + pkg)
+    
     if not os.path.exists('/var/opt'):
         os.symlink('/tmp/omsconfig/var/opt','/var/opt')
     if not os.path.exists('/etc/opt'):
         os.symlink('/tmp/omsconfig/etc/opt','/etc/opt')
+    if not os.path.exists('/opt/microsoft/'):
+        os.makedirs('/opt/microsoft/')
     if not os.path.exists('/opt/microsoft/omsconfig'):
-        os.makedirs('/opt/microsoft')
         os.symlink('/tmp/omsconfig/opt/microsoft/omsconfig','/opt/microsoft/omsconfig')
-
     if os.system('grep -q "omsagent:" /etc/group'):
         os.system('groupadd -r omsagent')
         os.system('touch /tmp/omstest_cleanup_group')
@@ -44,12 +50,11 @@ if not os.path.exists('/opt/micosoft/omsconfig'):
     os.system('cp /etc/sudoers /etc/sudoers.back')
     os.system('cat /tmp/omsconfig/etc/opt/microsoft/omsagent/sysconf/sudoers >> /etc/sudoers')
     os.system('chmod 440 /etc//sudoers')
-    testpath = '/opt/microsoft/omsconfig/Scripts/' + sys.argv[2] + '/Scripts/Tests/'
-    srcpath = '/SCX2/bld-omsagent/dsc/Providers/Scripts/' + sys.argv[2] + '/Scripts/Tests/ '
-    os.system('cp ' + srcpath + '*test_OMS_nxProviders.py ' + testpath)
-    os.system('cp ' + srcpath + '*dummy* ' + testpath)
+    destpath = '/opt/microsoft/omsconfig/Scripts/' + sys.argv[2]
     import pdb; pdb.set_trace()
-    os.chdir(testpath)
+    srcpath = '/' + os.path.join(*os.path.realpath(__file__).split('/')[:-2]) + '/*'
+    os.system('cp -r ' + srcpath + ' ' + destpath)
+    os.chdir(destpath)
     
     #Don't restart the service as it is not installed.
     os.system("echo '#!/bin/bash' > /tmp/omsconfig/opt/microsoft/omsagent/bin/service_control")
@@ -58,11 +63,11 @@ if not os.path.exists('/opt/micosoft/omsconfig'):
     #nxGroup
     os.system('/usr/sbin/groupadd -g 1101 jojomamas')
     #nxPackage
-    if os.system('which dpkg') == 0:
-        os.system('/usr/bin/dpkg -i ' + testpath + 'dummy-1.0.deb')
+    if os.system('which dpkg 2> /dev/null') == 0:
+        os.system('/usr/bin/dpkg -i ' + destpath + '/Scripts/Tests/dummy-1.0.deb')
     else:
-        os.system('/usr/bin/rpm -i ' + testpath + 'dummy-1.0.deb')
-result = os.system('su -c " python ' + testpath + 'test_OMS_nxProviders.py &> /tmp/test_nxOMSProviders_results.txt" omsagent')
+        os.system('/usr/bin/rpm -i ' + destpath + '/Scripts/Tests/dummy-1.0.deb')
+result = os.system('su -c " python ' + destpath + '/Scripts/Tests/test_OMS_nxProviders.py &> /tmp/test_nxOMSProviders_results.txt" omsagent')
 
 if os.path.exists('/tmp/omstest_cleanup'):
     os.system('rm -rf /var/opt')
@@ -74,7 +79,7 @@ if os.path.exists('/tmp/omstest_cleanup_user'):
     os.system('userdel omsagent')
 os.system('cp /etc/sudoers.back /etc/sudoers')
 os.system('chmod 440 /etc/sudoers')
-if os.system('which dpkg') == 0:
+if os.system('which dpkg 2> /dev/null') == 0:
     os.system('/usr/bin/dpkg -P dummy-1.0.deb')
 else:
     os.system('/usr/bin/rpm -e dummy-1.0.deb')
