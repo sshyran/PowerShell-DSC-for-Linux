3199dbba671de9d0086e197b56b8dae7a729d184
gauravga <gauravga@hotmail.com>
2016-10-04 12:56:07 -0700

    change inventory generation script

--- Providers/Scripts/3.x/Scripts/nxOMSInventoryMOF.py
+++ Providers/Scripts/3.x/Scripts/nxOMSInventoryMOF.py
@@ -15,114 +15,86 @@ nxDSCLog = imp.load_source('nxDSCLog', '../nxDSCLog.py')
 
 LG = nxDSCLog.DSCLog
 
-conf_path = '/etc/opt/microsoft/omsagent/conf/omsagent.d/customlog.conf'
+inventoryMof_path = '/etc/opt/microsoft/omsagent/conf/omsagent.d/inventory.mof'
 
-def init_vars(CustomLogObjects):
-    new_customlogs = []
-    if CustomLogObjects is not None :
-        for customlog in CustomLogObjects:
-            if customlog['LogName'].value is not None:
-                customlog['LogName']=customlog['LogName'].value
+def init_vars(Instances):
+    new_instances = []
+    if Instances is not None :
+        for instance in Instances:
+            if instance['InstanceName'].value is not None:
+                instance['InstanceName']=instance['InstanceName'].value
 
-            new_filepaths = []
-            if customlog['FilePath'] is not None and len(customlog['FilePath'].value) > 0:
-                for filepath in customlog['FilePath'].value:
-                    if filepath is not None and len(filepath) > 0:
-                        new_filepaths.append(filepath)
+            if instance['ClassName'].value is not None:
+                instance['ClassName']=instance['ClassName'].value
 
-            if len(new_filepaths) > 0:
-                customlog['FilePath'] = new_filepaths
-                new_customlogs.append(customlog)
+            new_properties = []
+            if instance['Properties'] is not None and len(customlog['Properties'].value) > 0:
+                for property in instance['Properties'].value:
+                    if property is not None and len(property) > 0:
+                        new_properties.append(property)
 
-    CustomLogObjects = new_customlogs
+            if len(new_properties) > 0:
+                instance['Properties'] = new_properties
+                new_instances.append(instance)
+
+    Instances = new_instances
     
-def Set_Marshall(Name, EnableCustomLogConfiguration = False, CustomLogObjects = None):
-    init_vars(CustomLogObjects)
-    Set(EnableCustomLogConfiguration, CustomLogObjects)
+def Set_Marshall(FilePath, Ensure = False, Instances = None):
+    init_vars(Instances)
+    Set(Ensure, Instances)
     return [0]
 
-def Test_Marshall(Name, EnableCustomLogConfiguration = False, CustomLogObjects = None):
-    init_vars(CustomLogObjects)
-    return Test(EnableCustomLogConfiguration, CustomLogObjects)
+def Test_Marshall(FilePath, Ensure = False, Instances = None):
+    init_vars(Instances)
+    return Test(Ensure, Instances)
 
-def Get_Marshall(Name, EnableCustomLogConfiguration = False, CustomLogObjects = None):
+def Get_Marshall(Properties, Ensure = False, Instances = None):
     arg_names = list(locals().keys())
-    init_vars(CustomLogObjects)
+    init_vars(Instances)
     
-    CurrentCustomLogObjects = Get(EnableCustomLogConfiguration, CustomLogObjects)
+    CurrentInstances = Instances
     Name = protocol.MI_String(Name)
-    EnableCustomLogConfiguration = protocol.MI_Boolean(EnableCustomLogConfiguration)
-    for customlog in CurrentCustomLogObjects:
-        customlog['LogName'] = protocol.MI_String(customlog['LogName'])
-        if customlog['FilePath'] is not None and len(customlog['FilePath']):
-            customlog['FilePath'] = protocol.MI_StringA(customlog['FilePath'])
-    CustomLogObjects = protocol.MI_InstanceA(CurrentCustomLogObjects)
+    Ensure = protocol.MI_Boolean(Ensure)
+    for instance in CurrentInstances:
+        instance['InstanceName'] = protocol.MI_String(instance['InstanceName'])
+        instance['ClassName'] = protocol.MI_String(instance['ClassName'])
+        if instance['Properties'] is not None and len(instance['Properties']):
+            instance['Properties'] = protocol.MI_StringA(instance['Properties'])
+    Instances = protocol.MI_InstanceA(CurrentInstances)
     retd = {}
     ld = locals()
     for k in arg_names:
         retd[k] = ld[k]
     return 0, retd
             
-def Set(EnableCustomLogConfiguration, CustomLogObjects):
-    if Test(EnableCustomLogConfiguration, CustomLogObjects) == [0]:
-        return [0]
-
-    UpdateConf(CustomLogObjects)
-
+def Set(Ensure, Instances):
+    GenerateInventoyMOF(Instances)
     return [0]
 
-def Test(EnableCustomLogConfiguration, CustomLogObjects):
-    if EnableCustomLogConfiguration:
-        CurrentCustomLogObjects = ReadConf()
-        if CurrentCustomLogObjects is None and CustomLogObjects is None:
-            return [0]
-        elif CurrentCustomLogObjects is None or CustomLogObjects is None:
-            return [-1]
-        
-        sorted(CustomLogObjects, key = lambda p:p['LogName'])
-        for customlog in CustomLogObjects:
-            customlog['FilePath'].sort()
+def Test(Ensure, Instances):
+    return [-1]
 
-        sorted(CurrentCustomLogObjects, key = lambda p:p['LogName'])
-        for customlog in CurrentCustomLogObjects:
-            customlog['FilePath'].sort()
+def GenerateInventoyMOF(Instances):
+    header = ''
+    footer= "\ninstance of OMI_ConfigurationDocument
+	     {
+                 DocumentType = "inventory";
+                 Version="2.0.0";
+                 MinimumCompatibleVersion = "2.0.0";
+                 Name="InventoryConfig";
+             };"
 
-        if CustomLogObjects != CurrentCustomLogObjects:
-            return [-1]
-
-    return [0]
-
-def Get(EnableCustomLogConfiguration, CustomLogObjects):
-    CurrentCustomLogObjects = ReadConf()
-    return CurrentCustomLogObjects 
-
-def ReadConf():
-    if not os.path.isfile(conf_path):
-        return [];
-    txt = codecs.open(conf_path, 'r', 'utf8').read()
-    customlog_src_srch_str=r'\n<source>\n  type tail.*?path (.*?)\n.*?pos_file /var/opt/microsoft/omsagent/state/(.*?)\.pos.*?tag oms\.blob\.CustomLog\.(.*?)\.\*.*?format none.*?</source>\n'
-    customlog_src_srch=re.compile(customlog_src_srch_str,re.M|re.S)
-    new_customlogs=[]
-    sources=customlog_src_srch.findall(txt)
-    for source in sources:
-        s_filepaths=[]
-        if len(source[0]):
-            s_filepaths = source[0].split(',')
-        logname=source[1]
-        new_customlogs.append({'FilePath':s_filepaths,'LogName':logname})
-    return new_customlogs
-            
-    
-def UpdateConf(CustomLogObjects):
-    header = '# this file is configured by the OMS service\n'
-    new_source = ''
-    if CustomLogObjects is not None:
-        for customlog in CustomLogObjects:
-            logname = customlog['LogName']
-            filepaths = ','.join(customlog['FilePath'])
-            new_source+='\n<source>\n  type tail\n  path ' + filepaths + '\n  pos_file /var/opt/microsoft/omsagent/state/' + logname + '.pos\n  read_from_head true\n  tag oms.blob.CustomLog.' + logname + '.*\n  format none\n</source>\n'
-    txt = header + new_source
-    if os.path.isfile(conf_path): 
-        shutil.copy2(conf_path, conf_path + '.bak')
-    codecs.open(conf_path, 'w', 'utf8').write(txt)
+    inventoryMofSection = ''
+    if Instances is not None:
+        for instance in Instances:
+            instanceName = instance['InstanceName']
+            className = instance['ClassName']
+            filepaths = ';\n'.join(instance['Properties'])
+            new_source = 'instance of ' + className + '\n{\n'
+            new_source+= filepaths + '\n};\n'
+            inventoryMofSection+=new_source
+    txt = header + inventoryMofSection + footer
+    if os.path.isfile(inventoryMof_path): 
+        shutil.copy2(inventoryMof_path, inventoryMof_path + '.bak')
+    codecs.open(inventoryMof_path, 'w', 'utf8').write(txt)
     os.system('sudo /opt/microsoft/omsagent/bin/service_control restart')
